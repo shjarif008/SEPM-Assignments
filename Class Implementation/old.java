e2448d05250f9552e57628d6834b369c1a89903a
shjarif008 <it23001@mbstu.ac.bd>
2026-01-18 13:44:26 +0600

    Create old.java

---
+++ Class Implementation/old.java
@@ -0,0 +1,11 @@
+class Pizza {
+    private String topping;
+
+    public Pizza(String topping) {
+        this.topping = topping;
+    }
+
+    public String getTopping() {
+        return topping;
+    }
+}
